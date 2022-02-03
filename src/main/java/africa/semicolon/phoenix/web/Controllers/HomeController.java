243716fd1cdf624ee111c5c64b82e7271a5da734
kindyluv <onyeukwuamara@gmail.com>
2022-02-03 14:45:00 +0100

    Added home controller

---
+++ src/main/java/africa/semicolon/phoenix/web/Controllers/HomeController.java
@@ -0,0 +1,14 @@
+package africa.semicolon.phoenix.web.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String showHomePage(){
+        return "home";
+    }
+}
