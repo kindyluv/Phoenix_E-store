f1a7adaa4189cd73ea737d7a3a6eca696765dc58
kindyluv <onyeukwuamara@gmail.com>
2022-02-04 11:19:39 +0100

    ....

--- src/main/java/africa/semicolon/phoenix/service/ProductService.java
+++ src/main/java/africa/semicolon/phoenix/service/ProductService.java
@@ -2,12 +2,17 @@ package africa.semicolon.phoenix.service;
 
 import africa.semicolon.phoenix.data.dto.ProductDto;
 import africa.semicolon.phoenix.data.models.Product;
+import africa.semicolon.phoenix.web.Exceptions.BusinessLogicException;
 import africa.semicolon.phoenix.web.Exceptions.ProductDoesNotExistException;
+import com.github.fge.jsonpatch.JsonPatch;
 
 import java.util.List;
 
 public interface ProductService {
     List<Product> getAllProducts();
     Product findProductById(Long productId) throws ProductDoesNotExistException;
-    Product createProduct(ProductDto productDto);
+    Product createProduct(ProductDto productDto) throws BusinessLogicException;
+    Product updateProductDetails(Long productId, JsonPatch patch) throws BusinessLogicException;
+//    Product updateProduct(Long productId, ProductDto productDto) throws ProductDoesNotExistException;
+
 }
