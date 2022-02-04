de230cf107e175e3a389f601ed0aae7eae0c7e0a
kindyluv <onyeukwuamara@gmail.com>
2022-02-04 11:19:17 +0100

    ....

--- src/main/java/africa/semicolon/phoenix/data/repository/ProductRepository.java
+++ src/main/java/africa/semicolon/phoenix/data/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package africa.semicolon.phoenix.data.repository;
 
+import africa.semicolon.phoenix.data.dto.ProductDto;
 import africa.semicolon.phoenix.data.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByName(String name);
+//    Product updateProductById(Long productId, ProductDto productDto);
 }
