9bd3e2102640fe5f55624ca9e27e0ea18a8025ed
kindyluv <onyeukwuamara@gmail.com>
2022-02-04 11:18:47 +0100

    ....

---
+++ src/main/java/africa/semicolon/phoenix/web/Exceptions/BusinessLogicException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.phoenix.web.Exceptions;
+
+public class BusinessLogicException extends Throwable {
+    public BusinessLogicException(String message) {
+        super(message);
+    }
+}
