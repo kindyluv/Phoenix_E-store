68a564a3fcb08fe06834ebc5651b947ba50c5d51
kindyluv <onyeukwuamara@gmail.com>
2022-02-17 16:20:09 +0100

    ....

---
+++ src/main/java/africa/semicolon/phoenix/service/cloud/CloudinaryService.java
@@ -0,0 +1,11 @@
+package africa.semicolon.phoenix.service.cloud;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public interface CloudinaryService {
+    Map<?,?> upload(byte[] bytes, Map<?,?> params) throws IOException;
+}
