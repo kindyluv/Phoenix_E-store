d7cfcc345c97e3ae72e7466e4ed58c8174734831
kindyluv <onyeukwuamara@gmail.com>
2022-02-17 16:19:15 +0100

    ....

---
+++ src/main/java/africa/semicolon/phoenix/data/models/Cart.java
@@ -0,0 +1,34 @@
+package africa.semicolon.phoenix.data.models;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cart {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @OneToMany(cascade= CascadeType.PERSIST, fetch= FetchType.EAGER)
+    private List<Item> itemList;
+
+    private Double totalPrice;
+
+    public void addItem(Item item){
+        if(itemList == null) itemList = new ArrayList<>();
+        itemList.add(item);
+    }
+}
