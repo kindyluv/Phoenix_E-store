922a5aa0be5ddd0165f77156425a8dbe49f381f9
kindyluv <onyeukwuamara@gmail.com>
2022-02-17 16:21:38 +0100

    ....

--- src/main/java/africa/semicolon/phoenix/data/repository/ProductRepository.java
+++ src/main/java/africa/semicolon/phoenix/data/repository/ProductRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
     Optional<Product> findByName(String name);
-//    Product updateProductById(Long productId, ProductDto productDto);
 }
