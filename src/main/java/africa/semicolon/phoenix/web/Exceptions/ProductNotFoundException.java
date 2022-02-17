b94d6132677231d9365f6b99d9dfdbdb69aa6e0a
kindyluv <onyeukwuamara@gmail.com>
2022-02-17 16:21:41 +0100

    ....

---
+++ src/main/java/africa/semicolon/phoenix/web/Exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.phoenix.web.Exceptions;
+
+public class ProductNotFoundException extends Throwable {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
