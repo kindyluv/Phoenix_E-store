95e7bce5007926dd6ac5e7863c8a4e39852f4c71
kindyluv <onyeukwuamara@gmail.com>
2022-02-18 16:49:58 +0100

    ....

--- src/main/java/africa/semicolon/phoenix/service/cart/CartService.java
+++ src/main/java/africa/semicolon/phoenix/service/cart/CartService.java
@@ -2,6 +2,7 @@ package africa.semicolon.phoenix.service.cart;
 
 import africa.semicolon.phoenix.data.dto.CartRequestDto;
 import africa.semicolon.phoenix.data.dto.CartResponseDto;
+import africa.semicolon.phoenix.data.dto.CartUpdateDto;
 import africa.semicolon.phoenix.data.models.Cart;
 import africa.semicolon.phoenix.web.Exceptions.BusinessLogicException;
 import africa.semicolon.phoenix.web.Exceptions.ProductNotFoundException;
@@ -10,4 +11,5 @@ import africa.semicolon.phoenix.web.Exceptions.UserNotFoundException;
 public interface CartService {
     CartResponseDto addItemToCart(CartRequestDto cart) throws UserNotFoundException, ProductNotFoundException, BusinessLogicException;
     Cart viewCart();
+    CartResponseDto updateCartItem(CartUpdateDto updateDto) throws UserNotFoundException, BusinessLogicException;
 }
