1b48bef9bd63205640b745d3debc059a7743edf7
kindyluv <onyeukwuamara@gmail.com>
2022-02-18 16:50:09 +0100

    ....

--- src/test/java/africa/semicolon/phoenix/service/cart/CartServiceImplTest.java
+++ src/test/java/africa/semicolon/phoenix/service/cart/CartServiceImplTest.java
@@ -2,6 +2,8 @@ package africa.semicolon.phoenix.service.cart;
 
 import africa.semicolon.phoenix.data.dto.CartRequestDto;
 import africa.semicolon.phoenix.data.dto.CartResponseDto;
+import africa.semicolon.phoenix.data.dto.CartUpdateDto;
+import africa.semicolon.phoenix.data.dto.QuantityOperation;
 import africa.semicolon.phoenix.data.models.AppUser;
 import africa.semicolon.phoenix.data.models.Cart;
 import africa.semicolon.phoenix.data.models.Item;
@@ -14,10 +16,14 @@ import africa.semicolon.phoenix.web.Exceptions.ProductNotFoundException;
 import africa.semicolon.phoenix.web.Exceptions.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,8 +44,14 @@ class CartServiceImplTest {
     @Autowired
     CartService cartService;
 
+    CartUpdateDto updateDto;
+
     @BeforeEach
     void setUp() {
+        updateDto = CartUpdateDto.builder()
+                .itemId(122L)
+                .quantityOp(QuantityOperation.INCREASE)
+                .userId(5005L).build();
     }
 
     @Test
@@ -52,9 +64,68 @@ class CartServiceImplTest {
         CartResponseDto cartResponseDto = cartService.addItemToCart(cartRequestDto);
         assertThat(cartResponseDto.getCartItems()).isNotNull();
         assertThat(cartResponseDto.getCartItems().size()).isEqualTo(1);
+
+        assertThat(cartResponseDto.getTotalPrice()).isNotEqualTo(0.0);
+        assertThat(cartResponseDto.getTotalPrice()).isEqualTo(500);
+    }
+
+    @Test
+    @DisplayName("Cart Price Updated test")
+    void updateCartPriceTest() throws UserNotFoundException, BusinessLogicException, ProductNotFoundException {
+        CartRequestDto cartRequestDto = new CartRequestDto();
+        cartRequestDto.setProductId(13L);
+        cartRequestDto.setUserId(5011L);
+        cartRequestDto.setQuantity(2);
+
+        CartResponseDto cartResponseDto = cartService.addItemToCart(cartRequestDto);
+        assertThat(cartResponseDto.getCartItems()).isNotNull();
+        assertThat(cartResponseDto.getCartItems().size()).isEqualTo(1);
+
+        assertThat(cartResponseDto.getTotalPrice()).isNotEqualTo(0.0);
+        assertThat(cartResponseDto.getTotalPrice()).isEqualTo(1000);
     }
 
     @Test
     void viewCart() {
+
+    }
+
+    @Test
+    @DisplayName("Update cart item quantity")
+    void updateCartItemTest() throws UserNotFoundException, BusinessLogicException {
+        AppUser appUser = appUserRepository.findById(updateDto.getUserId()).orElse(null);
+
+        assert appUser != null;
+        Cart userCart = appUser.getMyCart();
+//        assertThat(userCart.getItemList().size()).isEqualTo(2);
+        Item item = userCart.getItemList().get(0);
+        log.info("Item --> {}", item);
+        assertThat(item).isNotNull();
+        assertThat(item.getQuantityAddedToCart()).isEqualTo(2);
+        log.info("Cart update obj --> {}", userCart);
+//        get item
+//        for (int i = 0; i < userCart.getItemList().size(); i++) {
+//            item = userCart.getItemList().get(i);
+//            if (item.getId() == updateDto.getUserId()) break;
+//        }
+
+//        Predicate<Item> itemPredicate = i -> i.getId().equals(updateDto.getItemId());
+//        Optional<Item> optionalItem = userCart.getItemList().stream().filter(itemPredicate).findFirst();
+
+
+        CartResponseDto responseDto = cartService.updateCartItem(updateDto);
+        assertThat(responseDto.getCartItems()).isNotNull();
+        assertThat(responseDto.getCartItems().get(0).getQuantityAddedToCart()).isEqualTo(2);
+
+//        log.info("Cart update obj -> {}", updateDto);
+//
+//
+//        for (int i = 0; i < responseDto.getCartItems().size(); i++) {
+//            item = responseDto.getCartItems().get(i);
+//            if (item.getId() == updateDto.getUserId()) break;
+//        }
+//
+//        assertThat(item).isNotNull();
+//        assertThat(item.getQuantityAddedToCart()).isEqualTo(4);
     }
 }
