5cd7abfefa6abbd96f9428ff3f48831eec08b5bd
kindyluv <onyeukwuamara@gmail.com>
2022-02-18 16:50:20 +0100

    ....

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -7,7 +7,7 @@ truncate table cart_item_list;
 
 insert into product (id, name, price, quantity)
 values (12, 'Luxury Map', 2340, 3),
-       (13, 'MacBook Air', 18340, 4),
+       (13, 'MacBook Air', 500, 4),
        (14, 'Rocking chair', 5340, 5),
        (15, 'Purple T-shirt', 7340, 7);
 
