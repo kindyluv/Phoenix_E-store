b89872800e33e948042b2df5f015bccc0eaa5ce1
precious <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>
2022-02-23 14:08:42 +0100

    ...

---
+++ src/main/java/africa/semicolon/phoenix/security/RDBUserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package africa.semicolon.phoenix.security;
+
+import africa.semicolon.phoenix.data.models.AppUser;
+import africa.semicolon.phoenix.data.models.Authority;
+import africa.semicolon.phoenix.data.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RDBUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        query for user details from db
+        AppUser user = appUserRepository.findByEmail(username).orElse(null);
+//        check that user exists
+        if (user == null) throw new UsernameNotFoundException("Username does not exist");
+//        return UserDetails
+        return new User(user.getEmail(), user.getPassword(), getAuthorities(user.getAuthorities()));
+    }
+
+    private List<SimpleGrantedAuthority> getAuthorities(List<Authority> authorities) {
+        return authorities.stream()
+                .map(authority -> {
+                    return new SimpleGrantedAuthority(authority.name());
+                }).collect(Collectors.toList());
+    }
+}
