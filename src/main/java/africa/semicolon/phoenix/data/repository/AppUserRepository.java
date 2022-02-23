c1f57987373956a6d81d4c153ba7fceed6350063
precious <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>
2022-02-23 14:00:13 +0100

    ...

--- src/main/java/africa/semicolon/phoenix/data/repository/AppUserRepository.java
+++ src/main/java/africa/semicolon/phoenix/data/repository/AppUserRepository.java
@@ -3,5 +3,8 @@ package africa.semicolon.phoenix.data.repository;
 import africa.semicolon.phoenix.data.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
 }
